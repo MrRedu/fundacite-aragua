6412bb568ec5a01666bb973e67164476390f47b9
Eduardo <ejrb1234@gmail.com>
2024-03-09 18:03:54 -0400

    working on section component

--- src/components/atoms/ui/section/Section.jsx
+++ src/components/atoms/ui/section/Section.jsx
@@ -1,8 +1,15 @@
 import propTypes from 'prop-types'
 import styles from './Section.module.css'
-export const Section = ({ children }) => {
-  return <section className={styles.container}>{children}</section>
+export const Section = ({ children, isTheFirst }) => {
+  return (
+    <section
+      className={`${styles.container} ${isTheFirst && styles['is-the-first']}`}
+    >
+      {children}
+    </section>
+  )
 }
 Section.propTypes = {
+  isTheFirst: propTypes.bool,
   children: propTypes.node,
 }
