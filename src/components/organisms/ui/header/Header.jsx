38286305126c1dc10d848252e1d22425465aba72
Redu <ejrb1234@gmail.com>
2024-06-04 11:09:06 -0400

    🙌

--- src/components/organisms/ui/header/Header.jsx
+++ src/components/organisms/ui/header/Header.jsx
@@ -1,8 +1,21 @@
 import styles from './Header.module.css'
 export const Header = () => {
   return (
-    <header className={styles.header}>
-      <h1>FUNDACITE</h1>
+    <header id='inicio' className={styles.header}>
+
+      <a href="#inicio" className={styles['logo-link']}>
+        <img src="/fundacite-logo.jpg" alt="Fundacite Maracay Logo" className={styles.logo} />
+      </a>
+
+      <nav className={styles['nav-bar']}>
+        <ul className={styles['nav-list']}>
+          <li className={styles['item-list']}><a href="">¿Quiénes somos?</a></li>
+          <li className={styles['item-list']}><a href="">Proyectos</a></li>
+          <li className={styles['item-list']}><a href="">Inicio</a></li>
+        </ul>
+        <a href="#postular">Postular</a>
+      </nav>
+      
     </header>
   )
 }
