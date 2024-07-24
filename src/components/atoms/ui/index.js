b89b14cb502ad15e662c374bad590e9df35cebda
Eduardo <ejrb1234@gmail.com>
2024-07-24 11:15:10 -0400

    Initial commit ✅

--- src/components/atoms/ui/index.js
+++
@@ -1,3 +0,0 @@
-export * from './section/Section'
-export * from './embed/GoogleMapEmbed'
-export * from './scroll-to-top/ScrollToTop'
--- src/components/organisms/ui/index.js
+++
@@ -1,4 +0,0 @@
-export * from './accordion/Accordion'
-export * from './header/Header'
-export * from './footer/Footer'
-export * from './slider/Slider'
