98dfc82e68b3dce1ddeac4f6f1df0e152e865a93
Eduardo <ejrb1234@gmail.com>
2024-07-26 12:34:41 -0400

    GoogleMaps embed component + Alt imgs InfiniteScroll solved error

---
+++ src/app/components/atoms/ui/GoogleMapEmbed.jsx
@@ -0,0 +1,14 @@
+export const GoogleMapEmbed = () => {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3926.097771814699!2d-67.59807548993697!3d10.253698068576766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e803c9fe37f2a07%3A0x9f48fe709c9ff836!2sFundacite%20Aragua!5e0!3m2!1ses!2sve!4v1721854489811!5m2!1ses!2sve"
+      width="800"
+      height="600"
+      // style="border:0;"
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+      className="w-full h-96"
+    ></iframe>
+  )
+}
--- src/app/components/organisms/ui/InfiniteScrollHorizontal.jsx
+++ src/app/components/organisms/ui/InfiniteScrollHorizontal.jsx
@@ -4,8 +4,8 @@ export const InfiniteScrollHorizontal = ({ items }) => {
   return (
     <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
       <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
-        {items.map(({ logo, name }) => (
-          <li key={logo}>
+        {items.map(({ logo, name }, index) => (
+          <li key={index}>
             <Image src={logo} alt={name} width={180} height={80} />
           </li>
         ))}
@@ -14,8 +14,8 @@ export const InfiniteScrollHorizontal = ({ items }) => {
         className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
         aria-hidden="true"
       >
-        {items.map(({ logo, name }) => (
-          <li key={logo}>
+        {items.map(({ logo, name }, index) => (
+          <li key={index}>
             <Image src={logo} alt={name} width={180} height={80} />
           </li>
         ))}
