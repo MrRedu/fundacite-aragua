d61be3ab327da13dbf0aab315ac2b20471c9c822
Eduardo <ejrb1234@gmail.com>
2024-07-26 21:22:21 -0400

    NextAuth.js

--- src/app/components/organisms/ui/header/Header.jsx
+++ src/app/components/organisms/ui/header/Header.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useState } from 'react'
+import { LoginButton } from '../../../atoms/ui/LoginButton'
 
 export const Header = () => {
   const currentPathname = usePathname()
@@ -27,18 +28,19 @@ export const Header = () => {
             />
           </Link>
           <div className="flex items-center lg:order-2">
-            <Link
+            {/* <Link
               href="/login"
               className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
             >
               Ingresar
             </Link>
             <Link
-              href="/postulation"
+              href="/profile"
               className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
             >
               {`¡Postúlate!`}
-            </Link>
+            </Link> */}
+            <LoginButton />
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
