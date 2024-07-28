fb01c4072d98a587a603640c119ef05b484d06b6
Eduardo <ejrb1234@gmail.com>
2024-07-28 16:25:01 -0400

    AboutSection hecha un componente

--- src/app/(pages)/page.jsx
+++ src/app/(pages)/page.jsx
@@ -5,13 +5,13 @@ import {
   GoogleMapEmbed,
   BackToTopButton,
 } from '@/components/atoms/ui/'
+import { About } from '@/components/molecules/ui'
 import {
   InfiniteScrollHorizontal,
   Tabs,
   Carousel,
   CallToAction,
 } from '@/components/organisms/ui/'
-import Image from 'next/image'
 
 export default async function HomePage() {
   return (
@@ -21,46 +21,15 @@ export default async function HomePage() {
       </Section>
       <Section id={'about'}>
         <Title>{`¿Quiénes somos?`}</Title>
-        <h4 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{`Fundacite siembra de ciencia, tecnología e innovación.`}</h4>
-
-        <div className="flex flex-col xl:flex-row gap-8 mb-6">
-          <Image
-            src={'/robot.webp'}
-            alt={
-              'Robot perteneciente al Núcleo de Robótica Educativa “Dr. Clemente Herrera” inaugurado por Fundacite Aragua'
-            }
-            width={600}
-            height={400}
-            className="w-full h-auto object-cover rounded-lg"
-          />
-          <div className="w-full flex flex-col justify-center">
-            <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-              {`La Fundación para el desarrollo de la ciencia y la tecnología (FUNDACITE) Aragua, es una institución pública encargada de planificar, promover y coordinar el sistema de desarrollo científico-tecnológico en el estado Aragua.`}
-            </p>
-            <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-              {`El Ministerio del Poder Popular para Ciencia, Tecnología e Innovación mantiene su presencia en el estado Aragua, a través de su Unidad Territorial representada por la Fundación para el Desarrollo de la Ciencia y la Tecnología en el estado Aragua (Fundacite Aragua).`}
-            </p>
-            <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-              {`De esta manera se propicia la construcción de espacios favorables para
-            el encuentro entre el saber del pueblo y la investigación, el
-            conocimiento científico – tecnológico a través de acciones articuladas
-            con distintos actores, para la apropiación social en las comunidades.
-            A través de Fundacite Aragua se desarrollan diversos programas y
-            proyectos en toda la entidad, orientados a fortalecer el Sistema
-            Nacional de Ciencia, Tecnología e Innovación, con la misión de hacer
-            posible un nuevo Modelo Productivo Socialista.`}
-            </p>
-          </div>
-        </div>
-
+        <About />
         <Tabs items={TABS_ABOUT} />
       </Section>
       <Section id={'programs'}>
         <Title>{`Ejes de gestión`}</Title>
       </Section>
-      <Section notPadding>
+      <Section notPadding className="pb-12">
         <Section isDiv>
-          <Title>{`Entes adscritas`}</Title>
+          <Title>{`Entes adscritos`}</Title>
         </Section>
         <InfiniteScrollHorizontal items={ENTES_ADSCRITOS} />
       </Section>
