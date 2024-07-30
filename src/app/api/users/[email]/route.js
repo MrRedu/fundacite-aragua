b8e32332f48e9239ce2ba86b5ebe810df0bcc4b6
Eduardo <ejrb1234@gmail.com>
2024-07-30 17:29:56 -0400

    endpoint para actualizar usuario

--- src/app/api/users/[email]/route.js
+++ src/app/api/users/[email]/route.js
@@ -25,3 +25,56 @@ export async function GET(req, { params }) {
     )
   }
 }
+
+// Actualizar un usuario por email
+export async function PUT(req, { params }) {
+  try {
+    const {
+      names,
+      lastnames,
+      cedula,
+      birthdate,
+      phone,
+      email,
+      city,
+      address,
+      university,
+      career,
+    } = await req.json()
+
+    const [result] = await connection.query(
+      'UPDATE tbl_users SET names_user = ?, lastnames_user = ?, cedula_user = ?, birthdate_user = ?, phone_user = ?, email_user = ?, city_user = ?, address_user = ?, university_user = ?, career_user = ? WHERE email_user = ?',
+      [
+        names,
+        lastnames,
+        cedula,
+        birthdate,
+        phone,
+        email,
+        city,
+        address,
+        university,
+        career,
+        params.email,
+      ]
+    )
+
+    if (result.affectedRows === 0) {
+      return NextResponse.json({ message: 'User not found' }, { status: 404 })
+    }
+
+    return NextResponse.json(
+      { message: 'User updated successfully' },
+      { status: 200 }
+    )
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      {
+        message: error.message,
+        manualMessage: 'Error updating user',
+      },
+      { status: 500 }
+    )
+  }
+}
